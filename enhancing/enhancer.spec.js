84ed3e14b496600ae35dd1d16f9058268e69deb6
Tom Bauer <tbauer1898@gmail.com>
2020-10-14 13:02:52 -0500

    test one done

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,16 @@
+const { expect } = require('@jest/globals');
 const enhancer = require('./enhancer.js');
 // test away!
+
+let sword = {
+    name: 'Anduril',
+    durability: 65,
+    enhancement: 'Elven Runes'
+}
+
+test("repair() should restore durability of item to 100", () => {
+    
+    const repaired = enhancer.repair(sword)
+    expect(repaired.durability).toEqual(100)
+
+})
